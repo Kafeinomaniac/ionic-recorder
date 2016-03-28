29e9ac35645e781b4df7e2412ff10e5cbf52cc60
Doron Tal <doron@tracktunes.org>
2016-03-28 17:32:45 -0400

    minor

--- app/components/audio-player/audio-player.ts
+++ app/components/audio-player/audio-player.ts
@@ -21,16 +21,13 @@ export class AudioPlayer implements OnChanges {
     private time: string = '0:00';
     private duration: string = '0:00';
     private hidden: boolean = true;
+    private playPauseButtonIcon: string = 'play';
 
     constructor() {
         console.log('constructor():AudioPlayer');
     }
 
     ngOnInit() {
-    }
-
-    onClickPlayPauseButton() {
-
     }
 
     show() {
@@ -41,17 +38,61 @@ export class AudioPlayer implements OnChanges {
         this.hidden = true;
     }
 
+    play() {
+        console.log('AudioPlayer:play()');
+        this.playPauseButtonIcon = 'pause';
+    }
+
+    pause() {
+        console.log('AudioPlayer:pause()');
+        this.playPauseButtonIcon = 'play';
+        // this.sourceNode.disconnect();
+    }
+    
+    resume() {
+        console.log('AudioPlayer:resume()');
+        // this.sourceNode.connect(destination)
+    }
+    
+    stop() {
+        console.log('AudioPlayer:stop()');
+        this.playPauseButtonIcon = 'play';
+    }
+
+    onClickPlayPauseButton() {
+        console.log('onClickPlayPauseButton()');
+
+        if (this.playPauseButtonIcon === 'play') {
+            this.playPauseButtonIcon = 'pause';
+        }
+        else {
+            this.playPauseButtonIcon = 'play';
+        }
+    }
+
     onClickCloseButton() {
-        // TODO: stop the playing
+        this.stop();
         this.hide();
     }
 
     ngOnChanges(changeRecord: { [propertyName: string]: SimpleChange }) {
         console.log('AudioPlayer:ngOnChanges() title: ' + this.title);
         if (changeRecord['title']) {
+            if (!changeRecord['url']) {
+                console.error('title but no url');
+                alert('title but no url');
+                throw Error('title but no url');
+            }
             if (this.title !== undefined) {
                 this.show();
             }
+        }
+        if (changeRecord['url']) {
+            if (!changeRecord['title']) {
+                console.error('url but no title');
+                alert('url but no title');
+                throw Error('url but no title');
+            }            
         }
     }
 }
