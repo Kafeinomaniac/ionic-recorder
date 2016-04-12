f967cd20ed5b19fc2363c1b94f8351d782549d5e
Doron Tal <doron@tracktunes.org>
2016-04-12 05:32:09 -0400

    the progress-slider component is now working fine with events that are tested on both android and desktop chrome - it is fast!

--- app/app.ts
+++ app/app.ts
@@ -5,6 +5,9 @@ import {Type, enableProdMode, ExceptionHandler, provide} from 'angular2/core';
 import {AppState} from './providers/app-state/app-state';
 import {TabsPage} from './pages/tabs/tabs';
 import {DB_NAME} from './providers/local-db/local-db';
+// the reason for the following import 'es6-shim'; line is this:
+// https://forum.ionicframework.com/t/ionic-2-projects-updating-to-beta-4/49054
+import 'es6-shim';
 
 
 enableProdMode();
--- app/components/progress-slider/progress-slider.ts
+++ app/components/progress-slider/progress-slider.ts
@@ -13,14 +13,19 @@ import {IONIC_DIRECTIVES} from 'ionic-angular';
  */
 @Component({
     selector: 'progress-slider',
-    templateUrl: 'build/components/progress-slider/progress-slider.html',
-    directives: [IONIC_DIRECTIVES]
+    templateUrl: 'build/components/progress-slider/progress-slider.html'
+    // ,directives: [IONIC_DIRECTIVES]
 })
 export class ProgressSlider implements OnChanges {
     @Input() private position: number = 0.4;
 
-    constructor(private element: ElementRef) {
+    private trackClientXRange: { start: number, end: number };
 
+    private mouseUpListener: Function;
+    private mouseMoveListener: Function;
+
+    constructor(private element: ElementRef, private renderer: Renderer) {
+        console.log('constructor():ProgressSlider');
     }
 
     ngOnInit() {
@@ -33,7 +38,7 @@ export class ProgressSlider implements OnChanges {
         // this.trackWidth = getComputedStyle(this.element.nativeElement).getPropertyValue('width');
         */
     }
-    
+
     positionPercent() {
         return (100.0 * this.position).toString() + '%';
     }
@@ -42,9 +47,84 @@ export class ProgressSlider implements OnChanges {
         return (100.0 - 100.0 * this.position).toString() + '%';
     }
 
-    onHandleClick() {
-        alert('onHandleClick');
-        console.log('onHandleClick');
+    getTrackClientXRange(): { start: number, end: number } {
+        let width: number = parseFloat(getComputedStyle(
+            this.element.nativeElement, null)
+            .getPropertyValue('width').replace('px', '')),
+            offsetLeft: number = this.element.nativeElement.offsetLeft,
+            paddingLeft: number = parseFloat(getComputedStyle(
+                this.element.nativeElement.firstChild, null)
+                .getPropertyValue('padding-left').replace('px', ''));
+        return {
+            start: offsetLeft + paddingLeft,
+            end: offsetLeft + width - paddingLeft
+        }
+    }
+
+    jumpToPosition(clientX: number, range: { start: number, end: number }) {
+        // the next if-statement fixes a quirk observed in desktop Chrome
+        // where the ondrag event always ends up with a clientX value of 0
+        // as its last emitted value, even when that's clearly not the last
+        // location of dragging
+        if (clientX === 0) {
+            return;
+        }
+
+        let rangeX: number = range.end - range.start,
+            clickRelativeX: number = clientX - range.start;
+
+        if (clickRelativeX < 0) {
+            clickRelativeX = 0;
+        }
+
+        if (clickRelativeX > rangeX) {
+            clickRelativeX = rangeX;
+        }
+
+        this.position = clickRelativeX / rangeX;
+    }
+
+    onSliderMouseDown(event: MouseEvent) {
+        console.log('onSliderMouseDown ' + this.element.nativeElement);
+        this.trackClientXRange = this.getTrackClientXRange();
+        this.jumpToPosition(event.clientX, this.trackClientXRange);
+
+        this.mouseUpListener =
+            this.renderer.listenGlobal('body', 'mouseup',
+                (event: MouseEvent) => {
+                    this.onMouseUp(event);
+                });
+
+        this.mouseMoveListener =
+            this.renderer.listenGlobal('body', 'mousemove',
+                (event: MouseEvent) => {
+                    this.onMouseMove(event);
+                });
+    }
+
+    onMouseUp(event: MouseEvent) {
+        console.log('onMouseUp');
+        // free up the listening to mouse up from <body> now that it happened
+        // until the next time we click on the progress-bar
+        this.mouseUpListener();
+        this.mouseMoveListener();
+    }
+
+    onMouseMove(event: MouseEvent) {
+        this.jumpToPosition(event.clientX, this.trackClientXRange);
+    }
+
+    onSliderTouchMove(event: TouchEvent) {
+        // alert('on touch move  ' + event.touches[0].clientX);
+        this.jumpToPosition(event.touches[0].clientX, this.trackClientXRange);
+    }
+
+    onSliderTouchStart(event: TouchEvent) {
+        this.trackClientXRange = this.getTrackClientXRange();
+    }
+
+    onSliderTouchEnd(event: TouchEvent) {
+        
     }
 
     /**
