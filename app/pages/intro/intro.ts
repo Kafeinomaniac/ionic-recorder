575bf651942569fe63dd2e1a18b228b8d9a5285b
Doron Tal <doron@tracktunes.org>
2016-04-19 04:09:29 -0400

    minor

--- app/pages/intro/intro.ts
+++ app/pages/intro/intro.ts
@@ -15,19 +15,19 @@ export class TutorialPage {
     constructor(private nav: NavController, private menu: MenuController) {
         this.slides = [
             {
-                title: "Welcome to Tracktunes Inc's first open-source app",
-                description: "The <b>Ionic Recorder App</b> is a practical preview of the Ionic Framework in action, and a demonstration of proper code use.",
-                image: "img/ica-slidebox-img-1.png",
+                title: 'Ionic Recorder app, by <a href="https://tracktunes.org">Tracktunes Inc</a>',
+                description: 'The <b>Ionic Recorder App</b> is a practical preview of the Ionic Framework in action, and a demonstration of proper code use.',
+                image: 'img/ica-slidebox-img-1.png',
             },
             {
-                title: "What is Ionic?",
-                description: "<b>Ionic Framework</b> is an open source SDK that enables developers to build high quality mobile apps with web technologies like HTML, CSS, and JavaScript.",
-                image: "img/ica-slidebox-img-2.png",
+                title: 'What is Ionic?',
+                description: '<b>Ionic Framework</b> is an open source SDK that enables developers to build high quality mobile apps with web technologies like HTML, CSS, and JavaScript.',
+                image: 'img/ica-slidebox-img-2.png',
             },
             {
-                title: " What is Ionic Platform?",
-                description: "The <b>Ionic Platform</b> is a cloud platform for managing and scaling Ionic apps with integrated services like push notifications, native builds, user auth, and live updating.",
-                image: "img/ica-slidebox-img-3.png",
+                title: ' What is Ionic Platform?',
+                description: 'The <b>Ionic Platform</b> is a cloud platform for managing and scaling Ionic apps with integrated services like push notifications, native builds, user auth, and live updating.',
+                image: 'img/ica-slidebox-img-3.png',
             }
         ];
     }
