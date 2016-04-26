d20c5a3b43a09bc288667858f275285b7cbcf886
Doron Tal <doron@tracktunes.org>
2016-04-26 16:57:49 -0400

    0.0.7-alpha.2

---
+++ bin/commit.sh
@@ -0,0 +1,37 @@
+#/bin/sh
+
+# this is the only way we will commit from now on!
+# it automatically increments version numbers and automatically
+# tags each time.
+
+
+read MESSAGE
+
+if [ ! -e VERSION ]; then
+    echo "No VERSION file - are you running this from the project home dir?"
+    echo "Exiting ..."
+    exit 1
+fi
+
+VERSION="`cat VERSION`"
+NUM="`echo $VERSION | sed 's/.*\.//'`"
+
+NEWNUM="`echo $NUM "+1" | bc`"
+NEWVERSION="`echo $VERSION | perl -pe 's/(.*)\d+$/$1/'`$NEWNUM"
+
+echo "$VERSION -> $NEWVERSION"
+
+echo "STATUS:"
+git status
+
+echo -e "\n\nCommitting with comment:\n\n" $MESSAGE "\n\n"
+
+git add -A
+git commit -m $MESSAGE
+git push
+
+# now change VERSION and commit
+echo $NEWVERSION > VERSION
+
+git commit -m "$NEWVERSION"
+git push
