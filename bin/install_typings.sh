cf925dd2dcb511b02f8db75c7c46bd68ace29b16
Doron Tal <doron@tracktunes.org>
2016-06-03 14:32:57 -0400

    typings has changed in a way that broke everything changes fixed

--- bin/install_typings.sh
+++ bin/install_typings.sh
@@ -2,29 +2,27 @@
 
 # Copyright (C) 2016 Tracktunes Inc
 
-# Originally based on package.json at:
-#     https://github.com/lathonez/clicker/blob/master/package.json
-
 mkdir -p typings
 /bin/rm -fr typings/*
 
 APP_NAME="ionic-recorder"
 GITHUB_PATH="tracktunes/$APP_NAME"
 
+# created/modified typings from this project/app's repo
 for typing in \
     local=github:$GITHUB_PATH/typings/main/ambient/local/index.d.ts \
     waa=github:$GITHUB_PATH/typings/main/ambient/waa/index.d.ts \
     MediaStream=github:$GITHUB_PATH/typings/main/ambient/MediaStream/index.d.ts
 do
-    ./bin/typings install --ambient --save $typing
+    ./bin/typings install --global --save $typing
 done
 
+# dt (definitely typed) source typings
 for typing in \
     angular-protractor bluebird chalk del es6-shim express \
     express-serve-static-core glob gulp gulp-load-plugins gulp-typescript \
     gulp-util jasmine karma log4js mime minimatch node orchestrator q \
     run-sequence selenium-webdriver serve-static through2 vinyl
 do
-    ./bin/typings install --ambient --no-insight --save-dev $typing
+    ./bin/typings install --global --no-insight --save-dev dt~$typing
 done
-
