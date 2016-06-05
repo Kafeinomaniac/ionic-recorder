6f3c0a1021aaab8bc339ee8f9934cc5b2f618ffa
Doron Tal <doron@tracktunes.org>
2016-06-05 15:03:05 -0400

    added jsdoc heading

--- app/pages/loading/loading.ts
+++ app/pages/loading/loading.ts
@@ -1,6 +1,11 @@
 import {Page} from 'ionic-angular';
 // import {AppState} from '../../providers/app-state/app-state';
 
+/**
+ * @name LoadingPage
+ * @description
+ * Page that's displayed while we load things at app's start.
+ */
 @Page({
     templateUrl: 'build/pages/loading/loading.html'
 })
