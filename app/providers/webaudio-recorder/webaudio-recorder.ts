9369d635ce15be62b3a5627bdac584259ce9df22
Doron Tal <doron@tracktunes.org>
2016-06-16 04:18:41 -0400

    closes #87

--- app/providers/webaudio-recorder/webaudio-recorder.ts
+++ app/providers/webaudio-recorder/webaudio-recorder.ts
@@ -11,29 +11,36 @@ import {
 // sets the frame-rate at which either the volume monitor or the progress bar
 // is updated when it changes on the screen.
 const MONITOR_REFRESH_RATE_HZ: number = 24;
-
+// MONITOR_REFRESH_INTERVAL is derived from MONITOR_REFRESH_RATE_HZ
 const MONITOR_REFRESH_INTERVAL: number = 1000 / MONITOR_REFRESH_RATE_HZ;
 
-const AUDIO_CONTEXT: AudioContext = new (AudioContext || webkitAudioContext)();
+const AUDIO_CONTEXT: AudioContext = (function (): AudioContext {
+    if (typeof AudioContext === 'undefined') {
+        if (typeof webkitAudioContext === 'undefined') {
+            return null;
+        }
+        else {
+            return new webkitAudioContext();
+        }
+    }
+    else {
+        return new AudioContext();
+    }
+})();
 
 // length of script processing buffer (must be power of 2, smallest possible,
 // to reduce latency and to compute time as accurately as possible)
 const BUFFER_LENGTH: number = 256;
 
-// number of input channels and number of output channels
-const N_CHANNELS: number = 1;
-
-const NO_MICROPHONE_MSG: string = [
-    'This app needs the microphone to record audio with. Your browser got no ',
-    'access to your microphone - if this app is running on a desktop, ensure ',
-    'your microphone is connected (you may need to reload this page).'
-].join('');
-
-const NO_GETUSERMEDIA_MSG: string = [
-    'Your browser does not support the function getUserMedia(). Please ',
-    'upgrade to the latest version of Chrome (50 or above), if you want ',
-    'to use the recording part of this app.'
-].join('');
+// statuses
+export enum RecorderStatus {
+    UNINITIALIZED,
+    NO_CONTEXT,
+    NO_MICROPHONE,
+    NO_GETUSERMEDIA,
+    GETUSERMEDIA_ERROR,
+    READY
+}
 
 /**
  * @name WebAudioRecorder
@@ -52,7 +59,8 @@ export class WebAudioRecorder {
     // count # of buffers we have encoded (== time)
     private nEncodedBuffers: number;
 
-    public isReady: boolean;
+    public status: RecorderStatus;
+    public sampleRate: number;
     public isInactive: boolean;
     public isRecording: boolean;
     public currentVolume: number;
@@ -63,10 +71,9 @@ export class WebAudioRecorder {
 
     constructor() {
         console.log('constructor():WebAudioRecorder');
-        this.isReady = false;
+        this.status = RecorderStatus.UNINITIALIZED;
         this.intervalId = null;
         this.timeoutId = null;
-
         this.stop();
         this.resetPeaks();
         this.initAudio();
@@ -78,29 +85,30 @@ export class WebAudioRecorder {
      */
     private initAudio(): void {
         if (!AUDIO_CONTEXT) {
-            throw Error('AudioContext not available!');
+            this.status = RecorderStatus.NO_CONTEXT;
+            return;
         }
 
-        console.log('SAMPLE RATE: ' + AUDIO_CONTEXT.sampleRate);
+        this.sampleRate = AUDIO_CONTEXT.sampleRate;
+        console.log('SAMPLE RATE: ' + this.sampleRate);
 
         let getUserMediaOptions: Object = { video: false, audio: true };
 
-        if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+        if (typeof navigator !== 'undefined' &&
+            navigator.mediaDevices &&
+            navigator.mediaDevices.getUserMedia) {
             // new getUserMedia is available, use it to get microphone stream
-            console.log('Using NEW navigator.mediaDevices.getUserMedia');
+            // console.log('Using NEW navigator.mediaDevices.getUserMedia');
             navigator.mediaDevices.getUserMedia(getUserMediaOptions)
                 .then((stream: MediaStream) => {
                     this.setUpNodes(stream);
                 })
                 .catch((error: any) => {
-                    // this.stopWaitingForAudio();
-                    this.stopMonitoring();
-                    this.noMicrophoneAlert(error);
+                    this.status = RecorderStatus.NO_MICROPHONE;
                 });
         }
         else {
-            console.log('Using OLD navigator.getUserMedia (new not there)');
-            // new getUserMedia not there, try the old one
+            // console.log('Using OLD navigator.getUserMedia (new not there)');
             let getUserMedia: NavigatorGetUserMedia = navigator.getUserMedia ||
                 navigator.webkitGetUserMedia ||
                 navigator.mozGetUserMedia ||
@@ -114,36 +122,18 @@ export class WebAudioRecorder {
                             this.setUpNodes(stream);
                         },
                         (error: any) => {
-                            // this.stopWaitingForAudio();
-                            this.stopMonitoring();
-                            this.noMicrophoneAlert(error);
+                            this.status = RecorderStatus.NO_MICROPHONE;
                         });
                 }
                 catch (error) {
-                    alert('getUserMedia() Error: ' + error);
+                    this.status = RecorderStatus.GETUSERMEDIA_ERROR;
                 }
             }
             else {
                 // neither old nor new getUserMedia are available
-                alert(NO_GETUSERMEDIA_MSG);
+                this.status = RecorderStatus.NO_GETUSERMEDIA;
             }
         }
-    }
-
-    /**
-     * Show error message alert
-     * @param {any} error object with .name and .message fields
-     * @returns {void}
-     */
-    private noMicrophoneAlert(error: any): void {
-        console.log('noMicrophoneAlert(error): error = ' + error);
-        console.dir(error);
-        alert([
-            NO_MICROPHONE_MSG,
-            '\n\nError: ', error,
-            '\nError name: ', error.name,
-            '\nError message: ', error.message
-        ].join(''));
     }
 
     /**
@@ -151,36 +141,31 @@ export class WebAudioRecorder {
      * @returns {ScriptProcessorNode} the newly created/setup node
      */
     private makeScriptProcessorNode(): ScriptProcessorNode {
+        // NOTE: mono only (for now) 1 input channel 1, output channel
         let node: ScriptProcessorNode = AUDIO_CONTEXT.createScriptProcessor(
             BUFFER_LENGTH,
-            N_CHANNELS,
-            N_CHANNELS);
+            1,
+            1);
         node.onaudioprocess =
             (processingEvent: AudioProcessingEvent): any => {
                 // console.log('setUpNodes():onaudioprocess 1');
                 let inputBuffer: AudioBuffer = processingEvent.inputBuffer,
                     outputBuffer: AudioBuffer = processingEvent.outputBuffer,
-                    inputData: Float32Array,
-                    outputData: Float32Array,
-                    channel: number,
+                    inputData: Float32Array = inputBuffer.getChannelData(0),
+                    outputData: Float32Array = outputBuffer.getChannelData(0),
                     sample: number,
                     value: number,
                     absValue: number;
-                for (channel = 0; channel < N_CHANNELS; channel++) {
-                    inputData = inputBuffer.getChannelData(channel);
-                    outputData = outputBuffer.getChannelData(channel);
-                    value = 0;
-                    absValue = 0;
-                    this.currentVolume = 0;
-                    for (sample = 0; sample < BUFFER_LENGTH; sample++) {
-                        value = inputData[sample];
-                        absValue = Math.abs(value);
-                        if (absValue > this.currentVolume) {
-                            this.currentVolume = absValue;
-                        }
-                        outputData[sample] = value;
-                    } // for (sample ...
-                } // for (channel ...
+                this.currentVolume = 0;
+                for (sample = 0; sample < BUFFER_LENGTH; sample++) {
+                    value = inputData[sample];
+                    absValue = Math.abs(value);
+                    if (absValue > this.currentVolume) {
+                        this.currentVolume = absValue;
+                    }
+                    outputData[sample] = value;
+                } // for (sample ...
+
                 if (this.isRecording) {
                     this.nEncodedBuffers++;
                 }
@@ -225,32 +210,7 @@ export class WebAudioRecorder {
         this.startMonitoring();
 
         // and you can tell the world we're ready
-        this.isReady = true;
-    }
-
-    /**
-     * Compute the current latest buffer frame max volume and return it
-     * @returns {void}
-     */
-    private analyzeVolume(): void {
-        // we use currentVolume to represent current volume
-        // update some properties based on new value of currentVolume
-        this.nPeakMeasurements += 1;
-
-        // volume has changed, update this.currentVolume
-
-        if (this.currentVolume > this.maxVolumeSinceReset) {
-            // on new maximum, re-start counting peaks
-            this.resetPeaks();
-            this.maxVolumeSinceReset = this.currentVolume;
-        }
-        else if (this.currentVolume === this.maxVolumeSinceReset) {
-            this.nPeaksAtMax += 1;
-        }
-
-        this.percentPeaksAtMax =
-            (100 * this.nPeaksAtMax / this.nPeakMeasurements)
-                .toFixed(1);
+        this.status = RecorderStatus.READY;
     }
 
     ///////////////////////////////////////////////////////////////////////////
@@ -264,9 +224,25 @@ export class WebAudioRecorder {
     public startMonitoring(): void {
         this.intervalId = setInterval(
             () => {
-                this.analyzeVolume();
+                // update currentTime property
                 this.currentTime = formatTime(
                     this.nBuffersToSeconds(this.nEncodedBuffers));
+
+                // update currentVolume property
+                this.nPeakMeasurements += 1;
+                if (this.currentVolume > this.maxVolumeSinceReset) {
+                    // on new maximum, re-start counting peaks
+                    this.resetPeaks();
+                    this.maxVolumeSinceReset = this.currentVolume;
+                }
+                else if (this.currentVolume === this.maxVolumeSinceReset) {
+                    this.nPeaksAtMax += 1;
+                }
+
+                // update percentPeaksAtMax property
+                this.percentPeaksAtMax =
+                    (100 * this.nPeaksAtMax / this.nPeakMeasurements)
+                        .toFixed(1);
             },
             MONITOR_REFRESH_INTERVAL);
     }
@@ -304,13 +280,8 @@ export class WebAudioRecorder {
      * @returns {void}
      */
     public setGainFactor(factor: number): void {
-        if (!this.isReady) {
+        if (this.status !== RecorderStatus.READY) {
             return;
-        }
-        // we're ready
-        if (!this.audioGainNode) {
-            // but there is no gain node, very strange ...
-            throw Error('GainNode not initialized!');
         }
         this.audioGainNode.gain.value = factor;
     }
@@ -320,8 +291,7 @@ export class WebAudioRecorder {
      * @returns {number} Time in seconds
      */
     private nBuffersToSeconds(nBuffers: number): number {
-        return this.nEncodedBuffers * 256.0 /
-            AUDIO_CONTEXT.sampleRate;
+        return this.nEncodedBuffers * 256.0 / this.sampleRate;
     }
 
     /**
