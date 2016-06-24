e4271d21efe2da2b3e67368d4916aece1949daf7
Doron Tal <doron@tracktunes.org>
2016-06-24 15:20:25 -0400

    removed any dependence on arrays.ts, getting rid of it

--- app/services/utils/dictionary.spec.ts
+++ app/services/utils/dictionary.spec.ts
@@ -2,11 +2,6 @@ import {
     Dictionary
 } from './dictionary';
 
-import {
-    equals,
-    remove
-} from './arrays';
-
 let dict: Dictionary<any, any> = null,
     nElements: number = 100,
     elemKeys: any = [],
@@ -28,33 +23,33 @@ describe('Dictionary', () => {
     });
 
     it('Maps keys to values with string keys', () => {
-        expect(dict.getValue('sd')).toEqual(undefined);
+        expect(dict.getValue('sd')).toBe(undefined);
 
         // test with string keys
         for (i = 0; i < nElements; i++) {
-            expect(dict.setValue(elemKeys[i], i + 1)).toEqual(undefined);
+            expect(dict.setValue(elemKeys[i], i + 1)).toBe(undefined);
         }
-        expect(dict.size()).toEqual(nElements);
+        expect(dict.size()).toBe(nElements);
 
         for (i = 0; i < nElements; i++) {
-            expect(dict.getValue(elemKeys[i])).toEqual(i + 1);
+            expect(dict.getValue(elemKeys[i])).toBe(i + 1);
         }
 
         dict.setValue('a', 5);
-        expect(dict.getValue('a')).toEqual(5);
-        expect(dict.setValue('a', 21)).toEqual(5);
-        expect(dict.size()).toEqual(nElements + 1);
-        expect(dict.getValue('a')).toEqual(21);
+        expect(dict.getValue('a')).toBe(5);
+        expect(dict.setValue('a', 21)).toBe(5);
+        expect(dict.size()).toBe(nElements + 1);
+        expect(dict.getValue('a')).toBe(21);
     });
 
     it('Maps keys to values with number keys', () => {
         // test with number keys
         for (i = 0; i < nElements; i++) {
-            expect(dict.setValue(i, i + 1)).toEqual(undefined);
+            expect(dict.setValue(i, i + 1)).toBe(undefined);
         }
 
         for (i = 0; i < nElements; i++) {
-            expect(dict.getValue(i)).toEqual(i + 1);
+            expect(dict.getValue(i)).toBe(i + 1);
         }
     });
 
@@ -63,68 +58,68 @@ describe('Dictionary', () => {
             return obj.s;
         };
         dict = new Dictionary(ts);
-        expect(dict.getValue('sd')).toEqual(undefined);
+        expect(dict.getValue('sd')).toBe(undefined);
 
         for (i = 0; i < nElements; i++) {
             let o: any = {};
             o.s = elemKeys[i];
-            expect(dict.setValue(o, i + 1)).toEqual(undefined);
+            expect(dict.setValue(o, i + 1)).toBe(undefined);
         }
 
         for (i = 0; i < nElements; i++) {
             let d: any = {};
             d.s = elemKeys[i];
-            expect(dict.getValue(d)).toEqual(i + 1);
+            expect(dict.getValue(d)).toBe(i + 1);
         }
     });
 
     it('Removes existing elements from the dictionary', () => {
-        expect(dict.remove('1')).toEqual(undefined);
+        expect(dict.remove('1')).toBe(undefined);
         for (i = 0; i < nElements; i++) {
-            expect(dict.setValue(elemKeys[i], i + 1)).toEqual(undefined);
+            expect(dict.setValue(elemKeys[i], i + 1)).toBe(undefined);
         }
-        expect(dict.size()).toEqual(nElements);
+        expect(dict.size()).toBe(nElements);
         for (i = 0; i < nElements; i++) {
-            expect(dict.remove(elemKeys[i])).toEqual(i + 1);
-            expect(dict.getValue(elemKeys[i])).toEqual(undefined);
-            expect(dict.remove(elemKeys[i])).toEqual(undefined);
+            expect(dict.remove(elemKeys[i])).toBe(i + 1);
+            expect(dict.getValue(elemKeys[i])).toBe(undefined);
+            expect(dict.remove(elemKeys[i])).toBe(undefined);
         }
-        expect(dict.size()).toEqual(0);
+        expect(dict.size()).toBe(0);
     });
 
     it('An empty dictionary is empty', () => {
-        expect(dict.isEmpty()).toEqual(true);
+        expect(dict.isEmpty()).toBe(true);
         dict.setValue('1', 1);
-        expect(dict.isEmpty()).toEqual(false);
+        expect(dict.isEmpty()).toBe(false);
         dict.remove('1');
-        expect(dict.isEmpty()).toEqual(true);
+        expect(dict.isEmpty()).toBe(true);
     });
 
     it('Clear removes all elements', () => {
         dict.clear();
         dict.setValue(1, 1);
         dict.clear();
-        expect(dict.isEmpty()).toEqual(true);
-        expect(dict.getValue(1)).toEqual(undefined);
+        expect(dict.isEmpty()).toBe(true);
+        expect(dict.getValue(1)).toBe(undefined);
     });
 
     it('Contains existing keys', () => {
-        expect(dict.containsKey(0)).toEqual(false);
+        expect(dict.containsKey(0)).toBe(false);
         for (i = 0; i < 10; i++) {
             dict.setValue(elemKeys[i], i);
-            expect(dict.containsKey(elemKeys[i])).toEqual(true);
+            expect(dict.containsKey(elemKeys[i])).toBe(true);
         }
         for (i = 0; i < 10; i++) {
             dict.remove(elemKeys[i]);
-            expect(dict.containsKey(elemKeys[i])).toEqual(false);
+            expect(dict.containsKey(elemKeys[i])).toBe(false);
         }
     });
 
     it('Gives the right size', () => {
-        expect(dict.size()).toEqual(0);
+        expect(dict.size()).toBe(0);
         for (i = 0; i < 10; i++) {
             dict.setValue(elemKeys[i], i);
-            expect(dict.size()).toEqual(i + 1);
+            expect(dict.size()).toBe(i + 1);
         }
     });
 
@@ -135,7 +130,7 @@ describe('Dictionary', () => {
             keys = dict.keys();
             k.sort();
             keys.sort();
-            expect(equals(k, keys)).toEqual(true);
+            expect(k).toEqual(keys);
             dict.setValue(elemKeys[i], i);
             k.push(elemKeys[i]);
         }
@@ -148,26 +143,26 @@ describe('Dictionary', () => {
             values = dict.values();
             v.sort();
             values.sort();
-            expect(equals(v, values)).toEqual(true);
+            expect(v).toEqual(values);
             dict.setValue(elemKeys[i], i);
             v.push(i);
         }
     });
 
     it('For each gives all the pairs', () => {
-        let keys: any[],
-            values: any[];
+        let keys: Set<number>,
+            values: Set<number>;
         for (i = 0; i < nElements; i++) {
             dict.setValue(elemKeys[i], i);
         }
-        keys = dict.keys();
-        values = dict.values();
+        keys = new Set<number>(dict.keys());
+        values = new Set<number>(dict.values());
         dict.forEach((k: any, v: any) => {
-            expect(remove(keys, k)).toEqual(true);
-            expect(remove(values, v)).toEqual(true);
+            expect(keys.delete(k)).toBe(true);
+            expect(values.delete(v)).toBe(true);
         });
-        expect(keys.length).toEqual(0);
-        expect(values.length).toEqual(0);
+        expect(keys.size).toBe(0);
+        expect(values.size).toBe(0);
     });
 
     it('For each can be interrupted', () => {
@@ -179,7 +174,7 @@ describe('Dictionary', () => {
             t++;
             return false;
         });
-        expect(t).toEqual(1);
+        expect(t).toBe(1);
     });
 
 });
