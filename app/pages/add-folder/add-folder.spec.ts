cbe75bc8b06c6b7a1b6583d5f0f46d7d4568ade9
Doron Tal <doron@tracktunes.org>
2016-07-04 21:49:33 -0400

    added tests

--- app/pages/add-folder/add-folder.spec.ts
+++ app/pages/add-folder/add-folder.spec.ts
@@ -1,1 +1,32 @@
 // Copyright (c) 2016 Tracktunes Inc
+
+// import {
+//     describe,
+//     expect,
+//     it
+// } from '@angular/core/testing';
+
+// import {
+//     InstanceFixture,
+//     beforeEachDI
+// } from '../../services/test-utils/test-utils';
+
+// import {
+//     AddFolderPage
+// } from './add-folder';
+
+// let instanceFixture: InstanceFixture = null;
+
+// describe('pages/add-folder:AddFolderPage', () => {
+//     instanceFixture = beforeEachDI(
+//         AddFolderPage,
+//         [],
+//         true,
+//         null
+//     );
+
+//     it('initializes', () => {
+//         expect(instanceFixture.instance).not.toBeNull();
+//         expect(instanceFixture.fixture).not.toBeNull();
+//     });
+// });
