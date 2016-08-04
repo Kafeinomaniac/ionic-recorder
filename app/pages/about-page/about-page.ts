cfe759b76802da930cbce312b00af887d1c244db
dorontal <james_hendrix67@yahoo.com>
2016-08-04 14:01:54 -0400

    closes #114

--- app/pages/about-page/about-page.ts
+++ app/pages/about-page/about-page.ts
@@ -4,10 +4,6 @@ import {
     Component
 } from '@angular/core';
 
-import {
-    MenuController
-} from 'ionic-angular';
-
 const APP_VERSION: string = '0.0.8-alpha.19';
 
 /**
@@ -20,38 +16,14 @@ const APP_VERSION: string = '0.0.8-alpha.19';
     templateUrl: 'build/pages/about-page/about-page.html'
 })
 export class AboutPage {
-    private menuController: MenuController;
     private version: string;
 
     /**
      * AboutPage modal constructor
      */
-    constructor(menuController: MenuController) {
+    constructor() {
         console.log('constructor():AboutPage');
-        this.menuController = menuController;
         this.version = APP_VERSION;
-    }
-
-    /**
-     * https://webcake.co/page-lifecycle-hooks-in-ionic-2/
-     * @returns {void}
-     */
-    public ionViewDidEnter(): void {
-        // the left menu should be disabled on the tutorial page
-        this.menuController.enable(false);
-    }
-
-    /**
-     * UI callback handling cancellation of this modal
-     * @returns {void}
-     */
-    public onClickCancel(): void {
-        console.log('onClickCancel()');
-    }
-
-    public ionViewDidLeave(): void {
-        // enable the left menu when leaving the tutorial page
-        this.menuController.enable(true);
     }
 
 }
