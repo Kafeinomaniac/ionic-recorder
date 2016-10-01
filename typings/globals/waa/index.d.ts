0a3055afb5ef900e410a91aa1ee0a32cf4dc6be0
Doron Tal <doron@tracktunes.org>
2016-10-01 15:22:41 -0400

    in line with rc0 typescript compiler complaints - removed some unneeded declarations because they are now in built-in lib and slightly different than what was in this file

--- typings/globals/waa/index.d.ts
+++ typings/globals/waa/index.d.ts
@@ -329,14 +329,6 @@ interface MediaTrackSupportedConstraints {
 }
 
 interface MediaStream extends EventTarget {
-    id: string;
-    active: boolean;
-
-    onactive: EventListener;
-    oninactive: EventListener;
-    onaddtrack: (event: MediaStreamTrackEvent) => any;
-    onremovetrack: (event: MediaStreamTrackEvent) => any;
-
     clone(): MediaStream;
     stop(): void;
 
@@ -350,31 +342,15 @@ interface MediaStream extends EventTarget {
     removeTrack(track: MediaStreamTrack): void;
 }
 
-interface MediaStreamTrackEvent extends Event {
-    track: MediaStreamTrack;
-}
-
 declare enum MediaStreamTrackState {
 	"live",
     "ended"
 }
 
 interface MediaStreamTrack extends EventTarget {
-    id: string;
-    kind: string;
-    label: string;
     enabled: boolean;
-    muted: boolean;
-    remote: boolean;
-    readyState: MediaStreamTrackState;
-
-    onmute: EventListener;
-    onunmute: EventListener;
-    onended: EventListener;
-    onoverconstrained: EventListener;
 
     clone(): MediaStreamTrack;
-
     stop(): void;
 
     getCapabilities(): MediaTrackCapabilities;
@@ -384,39 +360,11 @@ interface MediaStreamTrack extends EventTarget {
 }
 
 interface MediaTrackCapabilities {
-    width: number | W3C.LongRange;
-    height: number | W3C.LongRange;
-    aspectRatio: number | W3C.DoubleRange;
-    frameRate: number | W3C.DoubleRange;
-    facingMode: string;
-    volume: number | W3C.DoubleRange;
-    sampleRate: number | W3C.LongRange;
-    sampleSize: number | W3C.LongRange;
-    echoCancellation: boolean[];
     latency: number | W3C.DoubleRange;
-    deviceId: string;
-    groupId: string;
 }
 
 interface MediaTrackSettings {
-    width: number;
-    height: number;
-    aspectRatio: number;
-    frameRate: number;
-    facingMode: string;
-    volume: number;
-    sampleRate: number;
-    sampleSize: number;
-    echoCancellation: boolean;
     latency: number;
-    deviceId: string;
-    groupId: string;
-}
-
-interface MediaStreamError {
-    name: string;
-    message: string;
-    constraintName: string;
 }
 
 interface NavigatorGetUserMedia {
@@ -448,8 +396,6 @@ interface MediaDevices {
 }
 
 interface MediaDeviceInfo {
-    label: string;
     id: string;
-    kind: string;
     facing: string;
 }
