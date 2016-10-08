ea532553a2e8245ce3ca5ed4b818c741965b41f6
Doron Tal <doron@tracktunes.org>
2016-10-08 16:05:11 -0400

    minor

--- scripts/scrape_audio_mime_types.sh
+++ scripts/scrape_audio_mime_types.sh
@@ -2,4 +2,4 @@
 
 URL="https://www.sitepoint.com/web-foundations/mime-types-complete-list/"
 
-curl $URL 2>&1 | grep 'audio/' | sed 's/^<td .*">//' | sed 's/<\/td>$//' | sort -u
+curl $URL 2>&1 | grep 'audio/' | sed 's/^<td .*">//' | sed 's/<\/td>$//' | sort -u | nl
