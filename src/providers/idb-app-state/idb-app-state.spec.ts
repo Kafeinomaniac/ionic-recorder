1fa1ca20e14ecbf5079bbdf670d2551779cc9989
Doron Tal <doron@tracktunes.org>
2017-02-01 14:20:25 -0500

    tests added, no page or app tests yet

---
+++ src/providers/idb-app-state/idb-app-state.spec.ts
@@ -0,0 +1,82 @@
+// Copyright (c) 2016 Tracktunes Inc
+
+import {
+    IdbAppState
+} from './idb-app-state';
+
+import {
+    isPositiveWholeNumber
+} from '../../models/utils/utils';
+
+const WAIT_MSEC: number = 60;
+
+let idbAppState: IdbAppState = new IdbAppState(),
+    savedTabIndex: number;
+
+beforeEach((done: Function) => {
+    idbAppState.waitForDB().subscribe(
+        (database: IDBDatabase) => {
+            done();
+        },
+        (error) => {
+            fail(error);
+        });
+});
+
+describe('When idbAppState initialized', () => {
+    it('idbAppState is not falsy', (done) => {
+        setTimeout(
+            () => {
+                expect(idbAppState).not.toBeFalsy();
+                done();
+            },
+            WAIT_MSEC);
+    });
+
+    it('can read lastTabIndex', (done) => {
+        setTimeout(
+            () => {
+                idbAppState.getProperty('lastTabIndex').subscribe(
+                    (idx: number) => {
+                        savedTabIndex = idx;
+                        expect(isPositiveWholeNumber(idx)).toBe(true);
+                        done();
+                    }
+                );
+            },
+            WAIT_MSEC);
+    });
+
+    it('can update lastTabIndex and read it', (done) => {
+        setTimeout(
+            () => {
+                idbAppState.updateProperty('lastTabIndex', 9999).subscribe(
+                    (bUpdated: boolean) => {
+                        expect(bUpdated).toBe(true);
+                        idbAppState.getProperty('lastTabIndex').subscribe(
+                            (prop: any) => {
+                                expect(prop).toBe(9999);
+                                done();
+                            });
+                    });
+            },
+            WAIT_MSEC);
+    });
+
+    it('can update lastTabIndex back to what it was', (done) => {
+        setTimeout(
+            () => {
+                idbAppState.updateProperty('lastTabIndex', savedTabIndex)
+                    .subscribe(
+                    (bUpdated: boolean) => {
+                        expect(bUpdated).toBe(true);
+                        idbAppState.getProperty('lastTabIndex').subscribe(
+                            (prop: any) => {
+                                expect(prop).toBe(savedTabIndex);
+                                done();
+                            });
+                    });
+            },
+            WAIT_MSEC);
+    });
+});
