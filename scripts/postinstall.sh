f2eb771d8fff11db33c8c0dec5421f3b74974443
Doron Tal <mountainbot@gmail.com>
2017-06-15 20:23:34 -0400

    using postinstall.sh

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-/bin/rm -fr platforms/browser && \
-cordova platform add browser && \
-cordova build && \
+# "postinstall": "ionic cordova platform add browser && cordova prepare && webdriver-manager update",
+
+/bin/rm -fr platforms/browser
+ionic cordova platform add browser
+cordova prepare 
+cordova build 
 webdriver-manager update
+
+
