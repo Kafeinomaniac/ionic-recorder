337ee04796787947b1e35f8803b032cea8b5caa1
Doron Tal <mountainbot@gmail.com>
2017-09-15 19:44:55 -0400

    name change

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,6 +1,6 @@
 // Copyright (c) 2017 Tracktunes Inc
 
-export * from './app-filesystem/app-filesystem';
+export * from './app-fs/app-fs';
 export * from './app-state/app-state';
 export * from './app-state/app-state.mock';
 export * from './master-clock/master-clock';
