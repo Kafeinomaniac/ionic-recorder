fee1350f1d999169d8a00137d835808fc43d205d
Doron Tal <mountainbot@gmail.com>
2017-10-28 13:18:49 -0400

    replaced ionic favicon with tracktunes favicon

--- src/pages/about-page/about-page.ts
+++ src/pages/about-page/about-page.ts
@@ -2,7 +2,7 @@
 
 import { Component } from '@angular/core';
 
-const APP_VERSION: string = '0.4.1-alpha.1';
+const APP_VERSION: string = '0.4.1-alpha.2';
 
 /**
  * Page that displays info about the app.
