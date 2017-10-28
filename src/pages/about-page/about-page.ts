5919469a9e99b54d04068f17756facaf411f4bac
Doron Tal <mountainbot@gmail.com>
2017-10-28 01:45:51 -0400

    now can save to local file on device

--- src/pages/about-page/about-page.ts
+++ src/pages/about-page/about-page.ts
@@ -2,7 +2,7 @@
 
 import { Component } from '@angular/core';
 
-const APP_VERSION: string = '0.4.0-alpha.8';
+const APP_VERSION: string = '0.4.0-alpha.9';
 
 /**
  * Page that displays info about the app.
