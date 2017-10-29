8638a9fbf6d61cdebb1ed9ddbf627daa3a6698b0
Doron Tal <mountainbot@gmail.com>
2017-10-29 00:36:30 -0400

    minor

--- src/pages/about-page/about-page.ts
+++ src/pages/about-page/about-page.ts
@@ -2,7 +2,7 @@
 
 import { Component } from '@angular/core';
 
-const APP_VERSION: string = '0.4.1-alpha.5';
+const APP_VERSION: string = '0.4.1-alpha.6';
 
 /**
  * Page that displays info about the app.
