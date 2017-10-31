b056dde941925c96e1e7429957895c113f84f37c
dorontal <doron@tracktunes.org>
2017-10-31 17:44:20 -0400

    tests fail but rename works... fix tests

--- src/pages/about-page/about-page.ts
+++ src/pages/about-page/about-page.ts
@@ -2,7 +2,7 @@
 
 import { Component } from '@angular/core';
 
-const APP_VERSION: string = '0.4.1-alpha.11';
+const APP_VERSION: string = '0.4.1-alpha.12';
 
 /**
  * Page that displays info about the app.
