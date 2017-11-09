f46a2eecfb8e073dc6629d40759b571d2525178f
Doron Tal <mountainbot@gmail.com>
2017-11-09 11:36:36 -0500

    got tests for page initializations

--- src/pages/about-page/about-page.ts
+++ src/pages/about-page/about-page.ts
@@ -2,7 +2,7 @@
 
 import { Component } from '@angular/core';
 
-const APP_VERSION: string = '0.5.0-alpha.9';
+const APP_VERSION: string = '0.5.0-alpha.10';
 
 /**
  * Page that displays info about the app.
