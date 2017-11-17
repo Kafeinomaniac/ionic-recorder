873806a776c3f059708e0067b3cd114195886ebf
dorontal <james_hendrix67@yahoo.com>
2017-11-17 11:42:37 -0500

    minor

--- scripts/list_sources.sh
+++ scripts/list_sources.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-GREPV='main\.ts|\.spec\.ts|\.d.ts|test\.ts|polyfills\.ts|mocks\.ts|index\.ts|\.mock\.ts'
+GREPV='main\.ts|\.spec\.ts|\.d.ts|test\.ts|polyfills\.ts|mocks\.ts|index\.ts|\.mock\.ts|\.module\.ts'
 
 find . -name \*\.ts | egrep -v $GREPV
